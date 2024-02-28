e10ba5d5d7a79c1cb480c08331da663955c6f29e
Sumit <sumit.kumar@bluebash.co>
2024-02-28 18:52:21 +0530

    minor code refactor

--- app/models/spree/stock_notify.rb
+++ app/models/spree/stock_notify.rb
@@ -1,4 +1,14 @@
 class Spree::StockNotify < ApplicationRecord
 	belongs_to :user
 	belongs_to :variant
+
+	validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, unless: :skip_email_validation?
+
+	private
+
+	def skip_email_validation?
+		debugger
+	  email.blank? && !user.blank?
+	end
+
 end
