33743ba921009d016a9a8bb68d69ca0900cbe6d5
Sumit <sumit.kumar@bluebash.co>
2024-03-11 18:52:37 +0530

    add ajax call

--- app/assets/javascripts/stock_notification_ajax.js
+++ app/assets/javascripts/stock_notification_ajax.js
@@ -1,25 +1,27 @@
-$(document).ready(function() {
-  $('#notify-me-form').submit(function(e) {
-    e.preventDefault(); // Prevent default form submission
-    
-    var formData = $(this).serialize(); // Serialize form data
-    var variantId = $('#notify-me-form input[name="variant_id"]').val(); // Get variant ID
-    
-    $.ajax({
-      dataType: 'json',
-      url: '/api/v2/storefront/products/' + variantId + '/stock_notify',
-      method: 'PUT',
-      data: formData,
-      success: function(response) {
-        // Handle success response
-        console.log(response);
-        $('#notify-me-form').replaceWith("<%= j render partial: 'spree/shared/notification' %>"); // Replace form with alert partial
-      },
-      error: function(xhr, status, error) {
-        // Handle error response
-        console.error(xhr.responseText);
-        alert('Failed to create stock notification');
-      }
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('notify-me-form').addEventListener('submit', function(e) {
+        e.preventDefault();
+        
+        var formData = new FormData(this); // Serialize form data
+        var variantId = document.getElementById('stock_notify_variant_id').value;
+        
+        $.ajax({
+            dataType: 'json',
+            url: `/api/v2/storefront/products/${variantId}/stock_notify`,
+            method: 'PUT',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(response) {
+              console.log(response);
+              var fetchDiv = document.getElementById('stock-notify-form')
+              fetchDiv.innerHTML = '<div class="mb-2 mt-3 text-bold">Thank you for your interest! We will notify you as soon as the product becomes available.</div>';
+            },
+            error: function(xhr, status, error) {
+              parsedError = JSON.parse(xhr.responseText);
+              alert(parsedError.errors);
+              document.getElementById('notify-me-button').removeAttribute('disabled')
+            }
+        });
     });
-  });
 });
