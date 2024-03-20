2c5c64923b5a5698651ae22c63ddce173ac40f92
Sumit <sumit.kumar@bluebash.co>
2024-03-20 15:53:15 +0530

    add fliper and apartment dependency inside the job.

--- app/jobs/stock_notify_job.rb
+++ app/jobs/stock_notify_job.rb
@@ -2,6 +2,14 @@ class StockNotifyJob < ApplicationJob
   queue_as :default
 
   def perform
-    StockNotifyService.run
+    Apartment.tenant_names.each do |tenant|
+      Apartment::Tenant.switch(tenant) do
+        Spree::Store.all.each do |store|
+          if Flipper.enabled?(:stock_notify, store.try(:id))
+            StockNotifyService.run
+          end
+        end
+      end
+    end
   end
 end
