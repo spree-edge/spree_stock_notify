9e8c369ff6eb1f7813b43a9ba1468b065d4e5c49
Sumit <sumit.kumar@bluebash.co>
2024-03-28 12:06:23 +0530

    Added extension specify module

--- app/models/spree/stock_notify.rb
+++ app/models/spree/stock_notify.rb
@@ -1,6 +1,6 @@
 class Spree::StockNotify < ApplicationRecord
   belongs_to :user, optional: true
-  belongs_to :variant
+  belongs_to :variant, touch: true
 
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, unless: :skip_email_validation?
   validates_uniqueness_of :email, scope: %i[variant_id notified], on: :create
